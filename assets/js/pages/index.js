27640d774957a4a765493fcb468fb3f654449128
Soumya Ranjan Jena <soumya.omitri@gmail.com>
2025-01-29 18:06:07 +0530

    Content & style change

--- assets/js/pages/index.js
+++ assets/js/pages/index.js
@@ -42,25 +42,25 @@ $(document).ready(function() {
 		itemsDesktop : [1199,5],
 		itemsDesktopSmall : [979,4]
     });
-	$(".read-more").on("click", function () {
-        var target = $(this).data("target");
-        var content = $("#" + target);
+	// $(".read-more").on("click", function () {
+    //     var target = $(this).data("target");
+    //     var content = $("#" + target);
 
-        content.toggleClass("expanded");
+    //     content.toggleClass("expanded");
 
-        $(".content").each(function () {
-            if ($(this).attr("id") !== target) {
-                $(this).removeClass("expanded");
-            }
-        });
-		$(".read-more").each(function(){
-			var btnTarget = $(this).data("target");
-			if(btnTarget === target && content.hasClass("expanded")){
-				$(this).text("See Les");
-			}else{
-				$(this).text("Read More")
-			}
-		});
-    });
+    //     $(".content").each(function () {
+    //         if ($(this).attr("id") !== target) {
+    //             $(this).removeClass("expanded");
+    //         }
+    //     });
+	// 	$(".read-more").each(function(){
+	// 		var btnTarget = $(this).data("target");
+	// 		if(btnTarget === target && content.hasClass("expanded")){
+	// 			$(this).text("See Les");
+	// 		}else{
+	// 			$(this).text("Read More")
+	// 		}
+	// 	});
+    // });
 
 });
