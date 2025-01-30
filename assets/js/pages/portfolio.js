75470d48df5ba72d221785aed24c7a803399ceef
Soumya Ranjan Jena <soumya.omitri@gmail.com>
2025-01-31 02:07:47 +0530

    All Content and Correction

--- assets/js/pages/portfolio.js
+++ assets/js/pages/portfolio.js
@@ -16,28 +16,28 @@ $(function(){
 	});
 
 
-	$('.zoom-gallery').magnificPopup({
-		delegate: 'a',
-		type: 'image',
-		closeOnContentClick: false,
-		closeBtnInside: false,
-		mainClass: 'mfp-with-zoom mfp-img-mobile',
-		image: {
-			verticalFit: true,
-			titleSrc: function(item) {
-				return item.el.attr('title');
-			}
-		},
-		gallery: {
-			enabled: true
-		},
-		zoom: {
-			enabled: true,
-			duration: 300, // don't foget to change the duration also in CSS
-			opener: function(element) {
-				return element.find('img');
-			}
-		}
+	// $('.zoom-gallery').magnificPopup({
+	// 	delegate: 'a',
+	// 	type: 'image',
+	// 	closeOnContentClick: false,
+	// 	closeBtnInside: false,
+	// 	mainClass: 'mfp-with-zoom mfp-img-mobile',
+	// 	image: {
+	// 		verticalFit: true,
+	// 		titleSrc: function(item) {
+	// 			return item.el.attr('title');
+	// 		}
+	// 	},
+	// 	gallery: {
+	// 		enabled: true
+	// 	},
+	// 	zoom: {
+	// 		enabled: true,
+	// 		duration: 300, // don't foget to change the duration also in CSS
+	// 		opener: function(element) {
+	// 			return element.find('img');
+	// 		}
+	// 	}
 		
-	});
+	// });
 });
